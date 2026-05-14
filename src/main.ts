9e37b38e34721eea1f1228e3d44b37dd831c0103
lvfonsecaa <141865548+lvfonsecaa@users.noreply.github.com>
2026-05-14 03:44:01 -0500

    sin versionamiento

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,12 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-
-  app.enableVersioning({
-    type: VersioningType.URI,
-    prefix: 'api/v',
-    defaultVersion: '1',
-  });
 
   await app.listen(3000);
 }
